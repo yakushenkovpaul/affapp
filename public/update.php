87a33f1b5259c171c7d744cdfd1da7f4a0b334ae
pavelandreev <yakushenkovpaul@gmail.com>
2017-12-05 17:53:38 +0300

    commit

---
+++ public/update.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: pavelandreev
+ * Date: 05.12.17
+ * Time: 17:45
+ */
+
+$path = '/var/www/html/affapp/';
+
+if(file_exists($path))
+{
+    $cmd = 'cd ' . $path . '; /usr/bin/git pull';
+    exec($cmd);
+    echo 'OK' . PHP_EOL;
+}
+else
+{
+    die('cant find path');
+}
