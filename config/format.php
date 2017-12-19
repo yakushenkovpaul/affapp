a0e71f3aff67695587c9231147c227d9643f1e14
pavelandreev <yakushenkovpaul@gmail.com>
2017-12-19 10:58:30 +0300

    commit

---
+++ config/format.php
@@ -0,0 +1,44 @@
+<?php
+
+return [
+
+	'encoding' => 'UTF-8',
+
+	'defaults' => [
+
+		'date'     => 'F j, Y',
+		'datetime' => 'F j, Y \a\t g:ia',
+		'time'     => 'g:ia',
+		'timezone' => null,
+
+		'phone' => [
+			'digits'              => 10,
+			'separator'           => '-',
+			'area_code_brackets'  => true,
+			'extension_separator' => ' x',
+			'strip_extension'     => false,
+		],
+
+		'bool_to_str_options' => 'Yes/No',
+
+		'string_limit' => [
+			'words'               => 50,
+			'chars'               => 140,
+			'trim'                => true,
+			'html'                => true,
+			'max_words_html'      => 100,
+			'max_chars_html'      => 480,
+			'exceeded_text'       => '...',
+			'exceeded_link_url'   => null,
+			'exceeded_link_class' => 'read-more',
+		],
+
+		'unique' => [
+			'model'       => false,
+			'char_limit'  => false,
+			'soft_delete' => false,
+		],
+
+	],
+
+];
