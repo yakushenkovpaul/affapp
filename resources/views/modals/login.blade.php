a1b463461e7fb44596e68b500fd981574a769ecd
dnuz <34270442+dnuz@users.noreply.github.com>
2017-12-27 13:32:38 +0100

    Update login.blade.php

--- resources/views/modals/login.blade.php
+++ resources/views/modals/login.blade.php
@@ -5,7 +5,7 @@
         <div class="modal-content">
             <div class="modal-header">
                 <button type="button" class="close" data-dismiss="modal">&times;</button>
-                <h4 class="modal-title">Log In to your Account</h4>
+                <h4 class="modal-title">Bei DonatIQ Anmelden</h4>
             </div>
             <div class="modal-body">
                 <form method="POST" action="{!! url('/login') !!}" class="loginForm" id="loginForm">
