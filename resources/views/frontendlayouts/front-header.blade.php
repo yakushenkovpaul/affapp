8915b3a34a5541f6df4240d0ea7722a85c31514c
pavelandreev <yakushenkovpaul@gmail.com>
2017-12-30 14:28:55 +0300

    commit

--- resources/views/frontendlayouts/front-header.blade.php
+++ resources/views/frontendlayouts/front-header.blade.php
@@ -7,17 +7,17 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Home - Listty</title>
     <!-- PLUGINS CSS STYLE -->
-    <link href="{{ asset('plugins/jquery-ui/jquery-ui.min.css?rnd=12') }}" rel="stylesheet">
-    <link href="{{ asset('plugins/bootstrap/css/bootstrap.min.css?rnd=12') }}" rel="stylesheet">
-    <link href="{{ asset('plugins/font-awesome/css/font-awesome.min.css?rnd=12') }}" rel="stylesheet">
-    <link href="{{ asset('plugins/listtyicons/style.css?rnd=12') }}" rel="stylesheet">
-    <link href="{{ asset('plugins/bootstrapthumbnail/bootstrap-thumbnail.css?rnd=12') }}" rel="stylesheet">
-    <link href="{{ asset('plugins/datepicker/datepicker.min.css?rnd=12') }}" rel="stylesheet">
-    <link href="{{ asset('plugins/selectbox/select_option1.css?rnd=12') }}" rel="stylesheet">
-    <link href="{{ asset('plugins/owl-carousel/owl.carousel.min.css?rnd=12') }}" rel="stylesheet">
-    <link href="{{ asset('plugins/fancybox/jquery.fancybox.pack.css?rnd=12') }}" rel="stylesheet">
-    <link href="{{ asset('plugins/isotope/isotope.min.css?rnd=12') }}" rel="stylesheet">
-    <link href="{{ asset('plugins/map/css/map.css?rnd=12') }}" rel="stylesheet">
+    <link href="{{ asset('plugins/jquery-ui/jquery-ui.min.css?rnd=16') }}" rel="stylesheet">
+    <link href="{{ asset('plugins/bootstrap/css/bootstrap.min.css?rnd=16') }}" rel="stylesheet">
+    <link href="{{ asset('plugins/font-awesome/css/font-awesome.min.css?rnd=16') }}" rel="stylesheet">
+    <link href="{{ asset('plugins/listtyicons/style.css?rnd=16') }}" rel="stylesheet">
+    <link href="{{ asset('plugins/bootstrapthumbnail/bootstrap-thumbnail.css?rnd=16') }}" rel="stylesheet">
+    <link href="{{ asset('plugins/datepicker/datepicker.min.css?rnd=16') }}" rel="stylesheet">
+    <link href="{{ asset('plugins/selectbox/select_option1.css?rnd=16') }}" rel="stylesheet">
+    <link href="{{ asset('plugins/owl-carousel/owl.carousel.min.css?rnd=16') }}" rel="stylesheet">
+    <link href="{{ asset('plugins/fancybox/jquery.fancybox.pack.css?rnd=16') }}" rel="stylesheet">
+    <link href="{{ asset('plugins/isotope/isotope.min.css?rnd=16') }}" rel="stylesheet">
+    <link href="{{ asset('plugins/map/css/map.css?rnd=16') }}" rel="stylesheet">
     <!-- GOOGLE FONT -->
 
     <link href="https://fonts.googleapis.com/css?family=Muli:200,300,400,600,700,800,900" rel="stylesheet">
@@ -26,10 +26,10 @@
     <link href="https://fonts.googleapis.com/css?family=Herr+Von+Muellerhoff" rel="stylesheet">
 
     <!-- CUSTOM CSS -->
-    <link href="{{ asset('css/style.css?rnd=12') }}" rel="stylesheet">
-    <link href="{{ asset('css/custom.css?rnd=12') }}" rel="stylesheet">
-    <link href="{{ asset('css/reviews.css?rnd=12') }}" rel="stylesheet">
-    <link href="{{ asset('css/frontend.css?rnd=12') }}" rel="stylesheet">
+    <link href="{{ asset('css/style.css?rnd=16') }}" rel="stylesheet">
+    <link href="{{ asset('css/custom.css?rnd=16') }}" rel="stylesheet">
+    <link href="{{ asset('css/reviews.css?rnd=16') }}" rel="stylesheet">
+    <link href="{{ asset('css/frontend.css?rnd=16') }}" rel="stylesheet">
     <!-- FAVICON -->
     <link href="{{ asset('img/favicon.png') }}" rel="shortcut icon">
 </head>
