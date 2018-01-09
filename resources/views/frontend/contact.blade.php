70b9c99bec42b3eb12b99b2a69ac4e5cae973748
dnuz <34270442+dnuz@users.noreply.github.com>
2018-01-09 21:08:41 +0100

    Update contact.blade.php

--- resources/views/frontend/contact.blade.php
+++ resources/views/frontend/contact.blade.php
@@ -22,32 +22,13 @@
         <div class="container">
             <div class="row">
                 <div class="col-sm-4 col-xs-12">
-                    <div>
-                        <img src="{{ asset('img/custom/contact.jpg') }}" alt="Contact us">
-                    </div>
-
-                    <div class="contactInfo">
-                        <ul class="list-unstyled list-address">
-                            <li>
-                                <i class="fa fa-map-marker" aria-hidden="true"></i>
-                                16/14 Babor Road, Mohammad pur <br> Dhaka, Bangladesh
-                            </li>
-                            <li>
-                                <i class="fa fa-phone" aria-hidden="true"></i>
-                                +55 654 545 122 <br> +55 654 545 123
-                            </li>
-                            <li>
-                                <i class="fa fa-envelope" aria-hidden="true"></i>
-                                <a href="#">info @example.com</a> <a href="#">info@startravelbangladesh.com</a>
-                            </li>
-                        </ul>
-                    </div>
+                    
                 </div>
                 <div class="col-sm-8 col-xs-12">
                     <div class="signUpFormArea">
                         <div class="priceTableTitle">
-                            <h2>Get inTouch</h2>
-                            <p>Please feel free to contact us if you have queries, require more information or have any other request.</p>
+                            <h2>Kontakt</h2>
+                            <p>Lasst uns wissen, was wir besser machen können und schreibt uns eine Nachricht.</p>
                         </div>
                         <div class="signUpForm">
                             <form method="POST" action="{!! url('/contact') !!}" id="contactForm">
@@ -58,27 +39,29 @@
                                             <label for="selectSome" class="control-label" id="contact_subject">Subject*</label>
                                             <div class="contactSelect">
                                                 <select name="contact_subject" class="select-drop" style="display: none;">
-                                                    <option value="">-- Select Subject --</option>
-                                                    <option value="Subject 1">Subject 1</option>
-                                                    <option value="Subject 2">Subject 2</option>
-                                                    <option value="Subject 3">Subject 3</option>
+                                                    <option value="">-- Betreff Wählen --</option>
+                                                    <option value="Subject 1">Meine Bestellung</option>
+                                                    <option value="Subject 2">Mein Klub</option>
+                                                    <option value="Subject 3">Mein Account</option>
+                                                    <option value="Subject 4">Verbesserungsvorschlag</option>
+                                                    <option value="Subject 5">Etwas anderes</option>
                                                 </select>
                                             </div>
                                         </div>
                                         <div class="form-group col-xs-12">
-                                            <label for="yourName" class="control-label">Your Name*</label>
+                                            <label for="yourName" class="control-label">Name*</label>
                                             <input type="text" class="form-control" id="contact_name" name="contact_name" value="{{ old('contact_name') }}">
                                         </div>
                                         <div class="form-group col-xs-12">
-                                            <label for="emailAddress" class="control-label">Email Address*</label>
+                                            <label for="emailAddress" class="control-label">E-Mail-Adresse*</label>
                                             <input type="email" class="form-control" id="contact_email" name="contact_email" value="{{ old('contact_email') }}">
                                         </div>
                                         <div class="form-group col-xs-12">
-                                            <label for="textBox" class="control-label">Text*</label>
+                                            <label for="textBox" class="control-label">Nachricht*</label>
                                             <textarea class="form-control" rows="3" id="contact_text" name="contact_text">{{ old('contact_text') }}</textarea>
                                         </div>
                                         <div class="form-group col-xs-12 mb0">
-                                            <button type="submit" class="btn btn-primary">Send Now</button>
+                                            <button type="submit" class="btn btn-primary">Absenden</button>
                                         </div>
                                     </div>
                                 </div>
