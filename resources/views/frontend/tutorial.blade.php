8de74d30806eccb522ea2c05536b946cc4a49858
dnuz <34270442+dnuz@users.noreply.github.com>
2018-01-10 14:44:30 +0100

    Update tutorial.blade.php

--- resources/views/frontend/tutorial.blade.php
+++ resources/views/frontend/tutorial.blade.php
@@ -6,11 +6,11 @@
     @include('frontendlayouts.front-top')
 
         <section class="clearfix pageTitleSection">
-            <div class="container">
+           <div class="container">
                 <div class="row">
                     <div class="col-xs-12">
                         <div class="pageTitle">
-                            <h2>how it works</h2>
+                            <h2>Wie es funktioniert</h2>
                         </div>
                     </div>
                 </div>
@@ -18,56 +18,93 @@
         </section>
 
 
-        <section class="clearfix howWorksSection">
-            <div class="container">
-                <div class="row">
-                    <div class="col-xs-12">
-                        <div class="howWorksInner">
-                            <div class="row">
-                                <div class="col-sm-4 col-sm-push-8 col-xs-12">
-                                    <div class="howWorksImage text-right"><img src="{{ asset('img/works/works-big-1.png') }}" alt="Image Works"></div>
-                                </div>
-                                <div class="col-sm-8 col-sm-pull-4 col-xs-12">
-                                    <div class="howWorksInfo text-left">
-                                        <p>Step 1</p>
-                                        <h3>Find what you want</h3>
-                                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed eiusmod tempor incididunt  labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident. sunt in culpa qui officia deserunt mollit anim id est laborum. Sed ut perspiciatis unde omnis iste natus error sit voluptatem </p>
-                                    </div>
-                                </div>
+       <section class="clearfix howWorksSection">
+    <div class="container">
+        <div class="row">
+            <div class="container pg-empty-placeholder"></div>
+            <div class="col-xs-2">
+</div>
+            <div class="col-xs-8">
+                <div class="howWorksInner">
+                    <div class="row">
+                        <div class="col-sm-4 col-sm-push-8 col-xs-12">
+                            <div class="howWorksImage text-right">
+                                <img src="img/works/lifebuoy.png" alt="Image Works">
                             </div>
                         </div>
-                        <div class="howWorksInner">
-                            <div class="row">
-                                <div class="col-sm-4 col-xs-12">
-                                    <div class="howWorksImage text-left"><img src="{{ asset('img/works/works-big-2.png') }}" alt="Image Works"></div>
-                                </div>
-                                <div class="col-sm-8 col-xs-12">
-                                    <div class="howWorksInfo text-right">
-                                        <p>Step 2</p>
-                                        <h3>Review &amp; Plan your day</h3>
-                                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed eiusmod tempor incididunt  labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident. sunt in culpa qui officia deserunt mollit anim id est laborum. Sed ut perspiciatis unde omnis iste natus error sit voluptatem </p>
-                                    </div>
-                                </div>
+                        <div class="col-sm-8 col-sm-pull-4 col-xs-12">
+                            <div class="howWorksInfo text-left">
+                                <h3>1. Entscheide Dich, wen Du unterstützt. </h3>
+                                <p>Finde Deinen Lieblingsverein füge diesen zu Deinen Favoriten. Falls Dein Verein bei uns fehlt oder Du gerne einer gemeinnützigen Organisation oder eine Schulverein helfen möchtest, scheib uns an wir fügen Deinen Verein auf unsere Seite.</p>
                             </div>
                         </div>
-                        <div class="howWorksInner">
-                            <div class="row">
-                                <div class="col-sm-4 col-sm-push-8 col-xs-12">
-                                    <div class="howWorksImage text-right"><img src="{{ asset('img/works/works-big-3.png') }}" alt="Image Works"></div>
-                                </div>
-                                <div class="col-sm-8 col-sm-pull-4 col-xs-12">
-                                    <div class="howWorksInfo text-left">
-                                        <p>Step 3</p>
-                                        <h3>Explore Location</h3>
-                                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed eiusmod tempor incididunt  labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident. sunt in culpa qui officia deserunt mollit anim id est laborum. Sed ut perspiciatis unde omnis iste natus error sit voluptatem </p>
-                                    </div>
-                                </div>
+                    </div>
+                </div>
+                <div class="howWorksInner">
+                    <div class="row">
+                        <div class="col-sm-4 col-xs-12">
+                            <div class="howWorksImage text-left">
+                                <img src="img/works/online-shop.png" alt="Image Works">
+                            </div>
+                        </div>
+                        <div class="col-sm-8 col-xs-12">
+                            <div class="howWorksInfo text-left">
+                                <h3>2. Besuche unsere Seite vor dem Kauf</h3>
+                                <p>Denke daran, unsere Seite immer vor dem Einkauf zu besuchen. Zum einen haben wir immer aktuelle Rabatte und Gutscheine, sodass Du sparen kannst und zum anderen und viel wichtiger ist, dass nur durch Besuchen unserer Seite wirst Du Deinem Verein helfen können, denn. Noch einfacher geht es mit unserem Browser Add-on</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="howWorksInner">
+                    <div class="row">
+                        <div class="col-sm-4 col-sm-push-8 col-xs-12">
+                            <div class="howWorksImage text-right">
+                                <img src="img/works/partner-shop.png" alt="Image Works">
+                            </div>
+                        </div>
+                        <div class="col-sm-8 col-sm-pull-4 col-xs-12">
+                            <div class="howWorksInfo text-left">
+                                <h3>3. Wähle einen Online Shop</h3>
+                                <p>Wir kooperieren mit Online Shops und erhalten von ihnen eine Provision solbald der Kauf erfolgreich abgeschlossen ist. Das heisst, jedes Mal, wenn Du über unser Service und unsere Partnerlinks etwas online kaufst, erhalten wir eine Provision vom Online Shop (sog. Cashback)</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="howWorksInner">
+                    <div class="row">
+                        <div class="col-sm-4 col-xs-12">
+                            <div class="howWorksImage text-left">
+                                <img src="img/works/euro.png" alt="Image Works">
+                            </div>
+                        </div>
+                        <div class="col-sm-8 col-xs-12">
+                            <div class="howWorksInfo text-left">
+                                <h3>4. Sammeln und ausschütten</h3>
+                                <p>Sobald Du und Deine Mitstreiter genügend Geld für den Verein gesammelt haben, überweisen wir das Geld an den Verein. Gemeinsam mit Euren Vereinsmitgliedern entscheidet Ihr, wofür Ihr das Geld am besten einsetzt. </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="howWorksInner">
+                    <div class="row">
+                        <div class="col-sm-4 col-sm-push-8 col-xs-12">
+                            <div class="howWorksImage text-right">
+                                <img src="img/works/conversation.png" alt="Image Works">
+                            </div>
+                        </div>
+                        <div class="col-sm-8 col-sm-pull-4 col-xs-12">
+                            <div class="howWorksInfo text-left">
+                                <h3>5. Weitersagen</h3>
+                                <p>Wenn viele Vereinsmitglieder mitmachen, wird Dein Verein mehr Geld sammeln. <br>
+Wenn es auch andere Vereine und ihre Vereinsmitglieder mitmachen, werden die Online Shopsuns uns bessere Gutscheine, Konditionen und auch höhere Provisionen bereitstellen. Weitersagen lohnt sich also. </p>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </section>
+        </div>
+    </div>
+</section>
 
 
     </div>
