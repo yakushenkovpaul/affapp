f04b47d2e96c6b420a54eefbc612174475cd3a77
dnuz <34270442+dnuz@users.noreply.github.com>
2018-01-13 18:11:02 +0100

    Update meta.blade.php

--- resources/views/user/meta.blade.php
+++ resources/views/user/meta.blade.php
@@ -1,16 +1,21 @@
 
     <div class="form-group col-xs-12">
-        @input_maker_label('Lastname')
+        @input_maker_label('Nachname')
         @input_maker_create('meta[lastname]', ['type' => 'string'], $user)
+    </div>
+    
+    <div class="form-group col-xs-12">
+        @input_maker_label('Anrede')
+        @input_maker_create('meta[gender]', ['type' => 'select', 'label' => 'gender', 'options' => [ 'Herr' => 'male', 'Frau' => 'female' ]], $user)
     </div>
 
     <div class="form-group col-xs-12">
-        @input_maker_label('City')
+        @input_maker_label('Stadt')
         @input_maker_create('meta[city]', ['type' => 'string'], $user)
     </div>
 
     <div class="form-group col-xs-12">
-        @input_maker_label('Birthday')
+        @input_maker_label('Geburtstag')
         @input_maker_create('meta[birthday]', ['type' => 'string' , 'class' => 'datepicker'], $user)
     </div>
 
@@ -20,17 +25,6 @@
         <input id="Meta[birthday]" class="form-control" type="text" name="meta[birthday]" value="09/06/1984" placeholder="Meta birthday">
     </div>
     -->
-
-    <div class="form-group col-xs-12">
-        @input_maker_label('Gender')
-        @input_maker_create('meta[gender]', ['type' => 'select', 'label' => 'gender', 'options' => [ 'Male' => 'male', 'Female' => 'female' ]], $user)
-    </div>
-
-
-    <div class="form-group col-xs-12">
-        @input_maker_label('Club')
-        @input_maker_create('meta[club_id]', ['type' => 'select', 'label' => 'club_id', 'options' => $clubs ], $user)
-    </div>
 
     <div class="form-group col-xs-12">
         @input_maker_label('Activate')
