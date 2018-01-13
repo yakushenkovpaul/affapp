3b6ddfb0cd432f5630770d5eea7fda7ce4345599
dnuz <34270442+dnuz@users.noreply.github.com>
2018-01-13 18:27:52 +0100

    Update favoritesClubs.blade.php

--- resources/views/user/favoritesClubs.blade.php
+++ resources/views/user/favoritesClubs.blade.php
@@ -11,7 +11,7 @@
                 <div class="row">
                     <div class="col-xs-12">
                         <div class="dashboardPageTitle">
-                            <h2>Favorites clubs</h2>
+                            <h2>Meine Lieblingsvereine</h2>
                         </div>
                         @if ($clubs->isEmpty())
                             <div class="well text-center">No clubs found.</div>
@@ -20,7 +20,7 @@
                                 <table id="ordersTable" class="table table-small-font table-bordered table-striped" cellspacing="0" width="100%">
                                     <thead>
                                     <tr>
-                                        <th>Club ID</th>
+                                        <th>ID</th>
                                         <th data-priority="1">Name</th>
                                         <th data-priority="2">Url</th>
                                         <th data-priority="3">Action</th>
@@ -28,7 +28,7 @@
                                     </thead>
                                     <tfoot>
                                     <tr>
-                                        <th>Club ID</th>
+                                        <th>ID</th>
                                         <th>Name</th>
                                         <th>Url</th>
                                         <th>Action</th>
@@ -45,7 +45,7 @@
                                                     {!! csrf_field() !!}
                                                     {!! method_field('DELETE') !!}
                                                     <div class="btn-group">
-                                                        <button type="submit" onclick="return confirm('Are you sure you want to delete this club?')" class="btn btn-primary">Delete</button>
+                                                        <button type="submit" onclick="return confirm('Bist Du sicher, dass Du diesen Verein aus Deiner Liste löschen willst?')" class="btn btn-primary">Löschen</button>
                                                     </div>
                                                 </form>
                                             </td>
