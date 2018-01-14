f5eec531224ccd18911a48185f6e10f84ff6f1bd
dnuz <34270442+dnuz@users.noreply.github.com>
2018-01-14 14:56:48 +0100

    Update password.blade.php

--- resources/views/user/password.blade.php
+++ resources/views/user/password.blade.php
@@ -14,22 +14,22 @@
 
                 <div class="raw-margin-top-24">
                     @input_maker_label('Old Password')
-                    @input_maker_create('old_password', ['type' => 'password', 'placeholder' => 'Old Password'])
+                    @input_maker_create('old_password', ['type' => 'password', 'placeholder' => 'Altes Passwort'])
                 </div>
 
                 <div class="raw-margin-top-24">
                     @input_maker_label('New Password')
-                    @input_maker_create('new_password', ['type' => 'password', 'placeholder' => 'New Password'])
+                    @input_maker_create('new_password', ['type' => 'password', 'placeholder' => 'Neues Passwort'])
                 </div>
 
                 <div class="raw-margin-top-24">
                     @input_maker_label('Confirm Password')
-                    @input_maker_create('new_password_confirmation', ['type' => 'password', 'placeholder' => 'Confirm Password'])
+                    @input_maker_create('new_password_confirmation', ['type' => 'password', 'placeholder' => 'Passwort bestätigen'])
                 </div>
 
                 <div class="raw-margin-top-24">
-                    <a class="btn btn-default pull-left" href="{{ URL::previous() }}">Cancel</a>
-                    <button class="btn btn-primary pull-right" type="submit">Save</button>
+                    <a class="btn btn-default pull-left" href="{{ URL::previous() }}">Abbrechen</a>
+                    <button class="btn btn-primary pull-right" type="submit">Speichern</button>
                 </div>
             </form>
         </div>
