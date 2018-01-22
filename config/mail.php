83b83463172bd4b2dbe67ee8ddb514e023e5654c
Paul <yakushenkovpaul@gmail.com>
2018-01-22 07:32:33 +0300

    da

--- config/mail.php
+++ config/mail.php
@@ -60,11 +60,12 @@ return [
         'name' => env('MAIL_FROM_NAME', 'Example'),
     ],
 
+    /*
     'to' => [
         'address' => env('MAIL_TO_ADDRESS', 'hello@example.com'),
         'name' => env('MAIL_FROM_NAME', 'Example'),
     ],
-
+    */
 
     /*
     |--------------------------------------------------------------------------
