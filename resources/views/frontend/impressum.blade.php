e3aa1015d493e85dde1e1af98061cc6bb1ebdac8
dnuz <34270442+dnuz@users.noreply.github.com>
2018-01-27 08:38:29 +0100

    Update impressum.blade.php

--- resources/views/frontend/impressum.blade.php
+++ resources/views/frontend/impressum.blade.php
@@ -25,27 +25,28 @@
                         <ul class="list-unstyled termsList">
                            
                             <li>
-                                <h3>Haftungsausschluss</h3>
+                                <h3></h3>
                                
                             </li>
                             <li>
-                                <h3>Inhalt des Onlineangebots</h3>
+                                <h3>Haftungsausschluss.</h3> <br>
+                                <h3>1. Inhalt des Onlineangebots</h3>
                                 <p>Der Autor übernimmt keinerlei Gewähr für die Aktualität, Korrektheit, Vollständigkeit oder Qualität der bereitgestellten Informationen. Haftungsansprüche gegen den Autor, welche sich auf Schäden materieller oder ideeller Art beziehen, die durch die Nutzung oder Nichtnutzung der dargebotenen Informationen bzw. durch die Nutzung fehlerhafter und unvollständiger Informationen verursacht wurden, sind grundsätzlich ausgeschlossen, sofern seitens des Autors kein nachweislich vorsätzliches oder grob fahrlässiges Verschulden vorliegt. Alle Angebote sind freibleibend und unverbindlich. Der Autor behält es sich ausdrücklich vor, Teile der Seiten oder das gesamte Angebot ohne gesonderte Ankündigung zu verändern, zu ergänzen, zu löschen oder die Veröffentlichung zeitweise oder endgültig einzustellen.</p>
                             </li>
                             <li>
-                                <h3>Verweise und Links</h3>
+                                <h3>2. Verweise und Links</h3>
                                 <p>Bei direkten oder indirekten Verweisen auf fremde Webseiten (”Hyperlinks”), die außerhalb des Verantwortungsbereiches des Autors liegen, würde eine Haftungsverpflichtung ausschließlich in dem Fall in Kraft treten, in dem der Autor von den Inhalten Kenntnis hat und es ihm technisch möglich und zumutbar wäre, die Nutzung im Falle rechtswidriger Inhalte zu verhindern. Der Autor erklärt hiermit ausdrücklich, dass zum Zeitpunkt der Linksetzung keine illegalen Inhalte auf den zu verlinkenden Seiten erkennbar waren. Auf die aktuelle und zukünftige Gestaltung, die Inhalte oder die Urheberschaft der verlinkten/verknüpften Seiten hat der Autor keinerlei Einfluss. Deshalb distanziert er sich hiermit ausdrücklich von allen Inhalten aller verlinkten /verknüpften Seiten, die nach der Linksetzung verändert wurden. Diese Feststellung gilt für alle innerhalb des eigenen Internetangebotes gesetzten Links und Verweise sowie für Fremdeinträge in vom Autor eingerichteten Gästebüchern, Diskussionsforen, Linkverzeichnissen, Mailinglisten und in allen anderen Formen von Datenbanken, auf deren Inhalt externe Schreibzugriffe möglich sind. Für illegale, fehlerhafte oder unvollständige Inhalte und insbesondere für Schäden, die aus der Nutzung oder Nichtnutzung solcherart dargebotener Informationen entstehen, haftet allein der Anbieter der Seite, auf welche verwiesen wurde, nicht derjenige, der über Links auf die jeweilige Veröffentlichung lediglich verweist.</p>
                             </li>
                             <li>
-                                <h3>Urheber- und Kennzeichenrecht</h3>
+                                <h3>3. Urheber- und Kennzeichenrecht</h3>
                                 <p>Der Autor ist bestrebt, in allen Publikationen die Urheberrechte der verwendeten Bilder, Grafiken, Tondokumente, Videosequenzen und Texte zu beachten, von ihm selbst erstellte Bilder, Grafiken, Tondokumente, Videosequenzen und Texte zu nutzen oder auf lizenzfreie Grafiken, Tondokumente, Videosequenzen und Texte zurückzugreifen. Alle innerhalb des Internetangebotes genannten und ggf. durch Dritte geschützten Marken- und Warenzeichen unterliegen uneingeschränkt den Bestimmungen des jeweils gültigen Kennzeichenrechts und den Besitzrechten der jeweiligen eingetragenen Eigentümer. Allein aufgrund der bloßen Nennung ist nicht der Schluss zu ziehen, dass Markenzeichen nicht durch Rechte Dritter geschützt sind! Das Copyright für veröffentlichte, vom Autor selbst erstellte Objekte bleibt allein beim Autor der Seiten. Eine Vervielfältigung oder Verwendung solcher Grafiken, Tondokumente, Videosequenzen und Texte in anderen elektronischen oder gedruckten Publikationen ist ohne ausdrückliche Zustimmung des Autors nicht gestattet.</p>
                             </li>
                             <li>
-                                <h3>Analytics</h3>
+                                <h3>4. Analytics</h3>
                                 <p>Diese Website benutzt Google Analytics, einen Webanalysedienst der Google Inc. („Google“) Google Analytics verwendet sog. „Cookies“, Textdateien, die auf Ihrem Computer gespeichert werden und die eine Analyse der Benutzung der Website durch Sie ermöglicht. Die durch den Cookie erzeugten Informationen über Ihre Benutzung diese Website (einschließlich Ihrer IP-Adresse) wird an einen Server von Google in den USA übertragen und dort gespeichert. Google wird diese Informationen benutzen, um Ihre Nutzung der Website auszuwerten, um Reports über die Websiteaktivitäten für die Websitebetreiber zusammenzustellen und um weitere mit der Websitenutzung und der Internetnutzung verbundene Dienstleistungen zu erbringen. Auch wird Google diese Informationen gegebenenfalls an Dritte übertragen, sofern dies gesetzlich vorgeschrieben oder soweit Dritte diese Daten im Auftrag von Google verarbeiten. Google wird in keinem Fall Ihre IP-Adresse mit anderen Daten der Google in Verbindung bringen. Sie können die Installation der Cookies durch eine entsprechende Einstellung Ihrer Browser Software verhindern; wir weisen Sie jedoch darauf hin, dass Sie in diesem Fall gegebenenfalls nicht sämtliche Funktionen dieser Website voll umfänglich nutzen können. Durch die Nutzung dieser Website erklären Sie sich mit der Bearbeitung der über Sie erhobenen Daten durch Google in der zuvor beschriebenen Art und Weise und zu dem zuvor benannten Zweck einverstanden.</p>
                             </li>
                              <li>
-                                <h3>Rechtswirksamkeit dieses Haftungsausschlusses</h3>
+                                <h3>5. Rechtswirksamkeit dieses Haftungsausschlusses</h3>
                                 <p>Dieser Haftungsausschluss ist als Teil des Internetangebotes zu betrachten, von dem aus auf diese Seite verwiesen wurde. Sofern Teile oder einzelne Formulierungen dieses Textes der geltenden Rechtslage nicht, nicht mehr oder nicht vollständig entsprechen sollten, bleiben die übrigen Teile des Dokumentes in ihrem Inhalt und ihrer Gültigkeit davon unberührt.</p>
                             </li>
                         </ul>
@@ -54,29 +55,24 @@
                         <ul class="list-unstyled termsList">
                             <li>
                                 <h3>Inhaltlich verantwortlich</h3>
-                                <p>Angaben gemäß § 5 TMG 
-                                Donatiq UG
-                                Geschäftsführer: 
-                                Wiener str 
-                                10999 Berlin</p>
+                                <p>Angaben gemäß § 5 TMG </p>
+                                <p>Donatiq UG</p>
+                                <p>Geschäftsführer: </p>
+                                <p> Wiener str </p>
+                                <p> 10999 Berlin</p>
                             </li>
                             <li>
-                                <h3>Kontakt</h3>
-                                <p><a href="contact.html">Kontaktformular</a></p>
+                                <p><b>Kontakt:</b> <a href="contact.html">Kontaktformular</a></p>
+                                <p>Anfragen werden <b>ausschließlich</b> über das Kontaktformular bearbeitet, wir bieten keinen Telefonsupport. Die Europäische Kommission stellt eine Plattform zur Online-Streitbeilegung bereit. Die Plattform finden Sie unter http://ec.europa.eu/consumers/odr/
+                                <p><b>Registergericht:</b></p>
+                                <p>Amtsgericht Charlottenburg, Berlin</p>
+                                <p><b>Handelsregisternummer:</b></p>
+                                <p>HRB 127132</p>
+                                <p><b>Ust-IdNr.:</b></p>
+                                <p>DE271592727</p>
                             </li>
-                            <li>
-                                <p>Anfragen werden ausschließlich über das Kontaktformular bearbeitet, wir bieten keinen Telefonsupport. Die Europäische Kommission stellt eine Plattform zur Online-Streitbeilegung bereit. Die Plattform finden Sie unter http://ec.europa.eu/consumers/odr/
-                                    Registergericht:
-                                    Amtsgericht Charlottenburg, Berlin
-                                    Handelsregisternummer:
-                                    HRB 127132
-                                    Ust-IdNr.:
-                                    DE271592727</p>
-                            </li>
-                            
                         </ul>
                     </div>
-                    
                 </div>
             </div>
         </section>
