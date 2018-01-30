6fae16fc8cc6fcebe413bf5b11ae035aac79c726
dnuz <34270442+dnuz@users.noreply.github.com>
2018-01-30 11:02:27 +0100

    Update agb.blade.php

--- resources/views/frontend/agb.blade.php
+++ resources/views/frontend/agb.blade.php
@@ -10,7 +10,7 @@
                 <div class="row">
                     <div class="col-xs-12">
                         <div class="pageTitle">
-                            <h2>Allgemeine Geschäftsbedingungen</h2>
+                            <h2>Nutzungsbedingungen</h2>
                         </div>
                     </div>
                 </div>
@@ -28,25 +28,64 @@
                             </li>
                             <li>
                                 <h3>1. Geltungsbereich</h3>
-                                <p>1.1. Diese Nutzungsbedingungen gelten sowohl für alle auf der Donatiq-Plattform registrierten Nutzer als auch für alle nicht registrierte Nutzer der Donatiq-Plattform (Besucher). Sie regeln ihre Rechte und Pflichten beim Besuch der donatiq-Plattform sowie bei der Nutzung der angebotenen Leistungen. Alle vom Service erbrachten Leistungen auf der Donatiq-Plattform werden ausschließlich auf Grundlage dieser Nutzungsbedingungen erbracht. Mit der Nutzung der donatiq-Plattform erklärt sich der Nutzer und Besucher mit diesen Nutzungsbedingungen einverstanden.
-.1.2. Ergänzend zu diesen Nutzungsbedingungen gelten die Datenschutzbestimmungen, sowie die Teilnahmebedingungen für Vereine. </p>
+                                <p>1.1 Diese Nutzungsbedingungen gelten sowohl für alle auf der DonatIQ-Plattform registrierten Nutzer als auch für alle nicht registrierte Nutzer der DonatIQ-Plattform (Besucher). Sie regeln ihre Rechte und Pflichten beim Besuch der DonatIQ-Plattform sowie bei der Nutzung aller angebotenen Leistungen. Alle vom Service erbrachten Leistungen auf der DonatIQ-Plattform werden ausschließlich auf Grundlage dieser Nutzungsbedingungen erbracht. Mit der Nutzung der DonatIQ-Plattform erklärt sich die Nutzer und Besucher mit diesen Nutzungsbedingungen einverstanden. Ergänzend zu diesen Nutzungsbedingungen gelten die Datenschutzbestimmungen, sowie die Teilnahmebedingungen für Vereine</p>
                             </li>
                             <li>
-                                <h3>2. . Geltungsbereich</h3>
-                                <p>We reserve the right to remove any business listings if we feel that they're infringing on any laws or if they contain objectionable material such as adult content (including images, vulgar language, content or links). In such cases, the business poster will receive a full refund for the listing.</p>
+                                <h3>2. Anmeldung</h3>
+                                <p>2.1 Das Angebot der DonatIQ-Plattform bedarf keiner Registrierung der Nutzer. Die wesentliche Funktion der Cashback Sammlung für den Verein ist für jeden Besucher möglich.</p> 
+                                <p>2.2 Die Registrierung als Nutzer auf DonatIQ-Plattform ist kostenfrei. Bei der Registrierung nimmt der Nutzer die Nutzungsbedingungen zur Kenntnis und akzeptiert diese durch die Aktivierung seines Nutzer-Accounts. Der Nutzer verpflichtet sich, abgefragten Angaben vollständig und wahrheitsgemäß anzugeben.</p>
+                                <p>2.3 Als Nutzer können Unternehmen oder natürliche Personen sein. Als natürliche Person muss der Nutzer das achtzehnte (18.) Lebensjahr vollendet haben. Minderjährige bedürfen zusätzlich der Einwilligung ihrer gesetzlichen Vertreter.</p>
+</p>
                             </li>
                             <li>
-                                <h3>Security and privacy</h3>
-                                <p>We make every possible attempt to keep your personal information safe, using the latest technology in Internet security. However, you should be aware that our site, as well as any other site, can potentially be a target for hackers. Please do not post any information on the site that is sensitive or that you would not want disclosed in case of an unlikely security breach. We use cookies to store information locally for advanced features of the site's operation, such as favorites, notes and alerts. Your personal details are not shared, rented or sold to any external companies or advertisers under any circumstances. Your credit card details and Paypal account details are only known to Paypal. You may review Paypal's agreements and policies for further information about how they safeguard your information. For security information, please visit: Paypal's security center .  For more information, please view Paypal's current <a href="#">privacy policy</a>.</p>
+                                <h3>3. Pflichten und Verantwortung des Nutzers</h3>
+                                <p>3.1 Dem Nutzer ist es unzulässig folgende Inhalte einzustellen, darauf zu verlinken oder sonst den Zugang dazu zu erleichtern:</p>
+                                <p>3.1.1 Jugendgefährdendes oder –beeinträchtigendes Material, insbesondere Gewaltverherrlichende, pornografische oder sonst sittenwidrige Inhalte,</p>
+                                <p>3.1.2 Inhalte, die Hass, Gewalt oder rassistische Diskriminierung fördern oder verherrlichen,</p>
+                                <p>3.1.3 unwahre Tatsachenbehauptungen oder Beleidigungen,</p>
+                                <p>3.1.4 vulgäre oder obszöne Ausdrücke, despektierlicher, provokativer und aggressiver Umgang mit anderen Mitgliedern,</p>
+                                <p>3.1.5 Irreführende Inhalte,</p>
+                                <p>3.1.6 Aufrufe zu Boykotten oder unfriedlichen bzw. sonst rechtswidrigen Handlungen.</p>
+                                <p>3.2 Der Nutzer ist verpflichtet, vor dem Einstellen von Inhalten auf DonatIQ-Plattform die erforderlichen Einwilligungen der Beteiligten, beispielsweise der Fotografen oder abgebildeten oder genannten Personen einzuholen.</p>
+                                <p>3.3 Soweit der Nutzer Inhalte auf der DonatIQ-Plattform einstellt, so räumt er hiermit dem Service alle erforderlichen Rechte ein, diese Inhalte frei und beliebig auf der DonatIQ-Plattform oder in Werbematerialien für die Plattform, on- und/oder offline zu verwenden, zu verändern, zu bearbeiten, zu speichern und öffentlich zugänglich zu machen.</p>
+                                <p>3.4 Es besteht kein Anspruch des Nutzers auf Auszahlung der Prämie an ihn selbst oder eine andere Private Person.</p>
+                           </li>
+                            <li>
+                                <h3>4. Leistungen und Pflichten von DonatIQ</h3>
+                                <p>4.1 DonatIQ steht als Vermittler eines (Kauf)Geschäfts zwischen Nutzer der DonatIQ-Plattform und den Partner-Unternehmen. Die jeweiligen Kauf-, Dienst-, Werk- oder sonstige Verträge kommen ausschließlich zwischen dem Nutzer und dem jeweiligen Partner-Unternehmen zustande. Die Vertragsabschlüsse daher unterliegen den Allgemeinen Geschäftsbedingungen (AGB) der Partner-Unternehmen. Zwischen DonatIQ und deren Nutzern entstehen durch das Rechtsgeschäft mit den Partner-Unternehmen keine Schuldverhältnisse, insbesondere keine vertraglichen Beziehungen bezüglich der von den Partner-Unternehmen angebotenen Produkte oder Dienstleistungen.</p>
+                                <p>4.2 DonatIQ zahlt Cashback an die Vereine in der Regel quartalsweise. Eine Cashback Zahlung an den jeweiligen Verein erfolgt nur insoweit, wie die dem Cashback zugrunde liegenden Provisionen innerhalb des jeweiligen Quartals vor Zahlung der Prämie bei DonatIQ eingegangen sind. Das Cashback wird an den jeweiligen Verein ausgezahlt, sofern alle notwendigen Informationen, wie Bankdaten hinterlegt sind. Es besteht kein Anspruch auf die Einhaltung eines festen Zahlungstermins.</p>
+                                <p>4.3 Die dargestellte Cashback Statistik im Nutzerprofil vom DonatIQ-Plattform kann sich verringern, wenn beispielsweise die Ware zurückgegeben bzw. die Online Transaktion widerrufen oder auf sonstige Weise das Geschäft rückabgewickelt wurde, oder das Partner-Unternehmen keine oder eine andere als die vereinbarte Provision auszahlt, oder das Partner-Unternehmen zwischenzeitlich seine Auszahlungsmodalitäten geändert hat. Dadurch kann entstandenes Cashback entfallen und verringern. Der Betrag des tatsächlich ausgezahlten Cashbacks kann sich von dem auf der DonatIQ-Plattform angezeigten Cashback unterscheiden. Die auf der DonatIQ-Plattform angegebene Höhe des Cashbacks ist ohne Gewähr.</p>
+                                <p>4.4 DonatIQ ist stets bemüht nach eigenem Ermessen berechtigte Ansprüche bzw. ausstehende Provisionen von den Partner-Unternehmen einzufordern. Allerdings gibt es seitens der Besucher, Nutzer oder Vereinen keinen Anspruch auf Auszahlung des Cashbacks. Die Nutzung der der DonatIQ-Plattform begründet nicht den Anspruch gegen DonatIQ auf Auszahlung der ausstehender Provisionen von Parnter-Unternehmen. Bei Provisionen, die DonatIQ zustehen, aber nicht von den Partner-Unternehmen gezahlt werden, besteht für DonatIQ keine Verpflichtung zur Zahlung von Cashback an Nutzer bzw. Vereine. Falls aus technischen Gründen ein Teil des Cashbacks einem Verein nicht zugeordnet werden kann, wird DonatIQ das Cashback nach eigenem Ermessen an einen anderen Verein oder mehrere Vereine auszahlen. Ein Anspruch auf konkrete Zuteilung der Prämie besteht nicht. Eine Überweisung von Prämien an die Vereine erfolgt ausschließlich bei Geschäften von Besuchern und Nutzern, die nachweislich über die DonatIQ-Plattform getätigt wurden.</p>
+                                <p>4.5 Falls von Partner-Unternehmen die Weitergabe von Cashback untersagt ist, bleibt das vom Nutzer generiertes Cashback in voller Höhe bei DonatIQ und wird für den Betrieb und die Weiterentwicklung der DonatIQ-Plattform verwendet.</p>
+                                <p>4.6 Alle Zahlungen von Zuwendungen an private Personen bzw. auf deren Konten sind ausgeschlossen. Der verantwortliche Ansprechpartner des jeweiligen Vereins muss sicherstellen, dass die Kontoverbindung des Vereins auf der DonatIQ-Plattform hinterlegt ist. Es besteht kein Anspruch des Nutzers auf Auszahlung der Prämie an ihn selbst.</p>
+                                <p>4.7 DonatIQ wird sich bemühen, die Plattform möglichst unterbrechungsfrei verfügbar zu halten. Eine bestimmte Verfügbarkeit wird nicht zugesagt. Insbesondere ist DonatIQ berechtigt, die DonatIQ-Plattform für Wartungsarbeiten aus dem Netz zu nehmen.</p>
+                                <p>4.8 DonatIQ ist berechtigt, Funktionalitäten, Aussehen oder andere Features der Plattform nach eigenem Belieben zu ändern. Zugang besteht nur zur Plattform in der jeweils aktuellen Version. Zur Aufbewahrung oder Speicherung von Versionen oder Inhalten ist Klubkasse gegenüber Nutzern nicht verpflichtet. </p>
+                                <p>4.9 DonatIQ ist dazu berechtigt, zu jeder Zeit unter Ausschluss sämtliche Schadenersatzansprüche die DonatIQ-Plattform mit sofortiger Wirkung ganz oder teilweise einzustellen und sämtliche gespeicherte Daten von Nutzern und Vereinen zu löschen.</p>
+
                             </li>
                             <li>
-                                <h3>Governing Law</h3>
-                                <p>These Terms shall be governed and construed in accordance with the laws of Ontario, Canada, without regard to its conflict of law provisions. Our failure to enforce any right or provision of these Terms will not be considered a waiver of those rights. If any provision of these Terms is held to be invalid or unenforceable by a court, the remaining provisions of these Terms will remain in effect. These Terms constitute the entire agreement between us regarding our Service, and supersede and replace any prior agreements we might have between us regarding the Service.</p>
+                                <h3>5. Haftung</h3>
+                                <p>5.1 DonatIQ haftet nicht für die Richtigkeit und den Inhalt der gemachten Angaben der Nutzer bezüglich des Vereins insbesondere nicht für die Aktualität, inhaltliche Richtigkeit und Vollständigkeit der Angaben zu den Vereinen. DonatIQ haftet nicht für den eventuellen Missbrauch dieser Angaben. DonatIQ haftet nicht für Inhalte von verlinkten Seiten.</p>
+                                <p>5.2 DonatIQ haftet nicht für die technische Fehler, die zu keinen Erfassung bzw. Zuordnung von Cashback zum Verein führen. Im Falle solcher Fehler ist DonatIQ bemüht, Cashback einer gemeinnützigen Organisation bzw. für einen gemeinnützigen Zweck zu verwenden. </p>
+                                <p>5.3 DonatIQ übernimmt keine Haftung hinsichtlich des Rechtsgeschäfts zwischen dem Benutzer bzw. Nutzer und dem Partner-Unternehmen. DonatIQ haftet auch nicht für die durch die Partner-Unternehmen angebotenen Waren und Dienstleistungen sowie für die Erfüllung der Verträge zwischen Nutzer und den Partner-Unternehmen sowie anderen Dienstleistern.</p>
+                                <p>5.4 DonatIQ haftet nicht für die unbefugte Kenntniserlangung von Daten der Nutzer und Vereine durch Dritte. DonatIQ haftet nicht für einen Missbrauch von Angaben und Informationen von Seiten Dritter, welche die Nutzer selbst diesen Dritten zugänglich gemacht haben.</p>
+                                <p>5.5 DonatIQ haftet nicht für wirtschaftliche oder immaterielle Schäden, die sich aus der Nutzung der DonatIQ-Plattform ergeben, soweit der Schaden nicht nachweislich vorsätzlich oder durch grobe Fahrlässigkeit des Betreibers verursacht wurde.</p>
+                                <p>5.6 DonatIQ haftet nicht für die auf der DonatIQ-Plattform dargestellten Produktinformationen Dienstleistungen von Partner-Unternehmen, die zu keinen verbindlichen Angeboten von DonatIQ zählen.</p>
+                                <p>5.7 DonatIQ übernimmt keine Gewähr für die Aktualität, Richtigkeit, Vollständigkeit der von den Partner-Unternehmen bereitgestellten Informationen.</p>
                             </li>
                             <li>
-                                <h3>Contact Us</h3>
-                                <p>f you have any questions, concerns, or suggestions regarding this terms, please <a href="contact-us.html">contact us</a>.</p>
+                                <h3>6. Änderungen der Nutzungsbedingungen</h3>
+                                <p>6.1 Die vorliegenden Nutzungsbedingungen können von DonatIQ ohne Angabe von Gründen jederzeit geändert werden. DonatIQ wird dies mit einer Frist von mindestens zwei Wochen zum Änderungszeitpunkt durch Mitteilung in Textform (z.B. E-Mail) ankündigen. Der Nutzer ist dann berechtigt, das Nutzungsverhältnis bis zum Inkrafttreten der Änderung oder Ergänzung mit sofortiger Wirkung zu beenden. Das Nutzungsverhältnis endet mit Zugang des Widerspruchs automatisch, ohne dass es einer weiteren Beendigungserklärung von DonatIQ bedarf. Widerspricht der Nutzer innerhalb der zwei Wochen nach Empfang der Mitteilung den Änderungen nicht ausdrücklich (per E-Mail oder Brief), gelten die neuen modifizierten Nutzungsbedingungen als angenommen.</p>
+                              
                             </li>
+                            <li>
+                                <h3>7. Salvatorische Klausel (Erhaltungsklausel)</h3>
+                                <p>7.1 Sollte eine Regelung dieser Nutzungsbedingungen unwirksam sein oder werden, so wird dadurch die Gültigkeit der übrigen Regelungen nicht berührt. An die Stelle der unwirksamen Bestimmung tritt eine wirksame, wie sie die Vertragsparteien bei billiger Berücksichtigung der beiderseitigen wirtschaftlichen Interessen zum Zeitpunkt des Vertragsschlusses vereinbart hätten, wenn ihnen die Unwirksamkeit der Bestimmung bekannt gewesen wäre. Entsprechendes gilt für die Ausfüllung von Lücken, die sich in diesem Vertrag etwa herausstellen könnten.</p>
+                            </li>
+                            <li>
+                                <h3>8. Anwendbares Recht</h3>
+                                <p>Es gilt das Recht der Bundesrepublik Deutschland unter Ausschluss des UN-Kaufrechts.</p>
+                            </li>                            
                         </ul>
                     </div>
                 </div>
