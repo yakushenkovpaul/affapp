fbbed75e8ada390760da22f9dcea73c739a1e4f6
dnuz <34270442+dnuz@users.noreply.github.com>
2018-02-12 11:12:27 +0100

    Update front-header.blade.php

--- resources/views/layouts/frontend/front-header.blade.php
+++ resources/views/layouts/frontend/front-header.blade.php
@@ -6,7 +6,14 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf_token" content="{{ csrf_token() }}" />
-    <title>Home - Listty</title>
+    <title>DonatIQ - Der wohl smarteste Weg zum zusätzlichen Finanzieren Deines Vereins</title>
+    <!-- Google Tag Manager -->
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-M8TR6C6');</script>
+    <!-- End Google Tag Manager -->
     <!-- PLUGINS CSS STYLE -->
     <link href="{{ asset('plugins/jquery-ui/jquery-ui.min.css?rnd=28') }}" rel="stylesheet">
     <link href="{{ asset('plugins/bootstrap/css/bootstrap.min.css?rnd=28') }}" rel="stylesheet">
