6a2302fea9f2ba2630a0e623db972f37c6a7e1dc
dnuz <34270442+dnuz@users.noreply.github.com>
2018-02-13 11:36:06 +0100

    Update datenschutz.blade.php

--- resources/views/frontend/datenschutz.blade.php
+++ resources/views/frontend/datenschutz.blade.php
@@ -22,31 +22,90 @@
             <div class="container">
                 <div class="row">
                     <div class="col-xs-12">
-                        <ul class="list-unstyled termsList">
-                            <li>
-                                <p>To advertise your business please ensure that you read and abide by the following terms and conditions.</p>
-                            </li>
-                            <li>
-                                <h3>Fair use of site</h3>
-                                <p>We are not responsible for any damages caused by the use of this website, or by posting business listings here. Please use our site at your own discretion and exercise good judgement as well as common sense when advertising business here.</p>
-                            </li>
-                            <li>
-                                <h3>Business deletion</h3>
-                                <p>We reserve the right to remove any business listings if we feel that they're infringing on any laws or if they contain objectionable material such as adult content (including images, vulgar language, content or links). In such cases, the business poster will receive a full refund for the listing.</p>
-                            </li>
-                            <li>
-                                <h3>Security and privacy</h3>
-                                <p>We make every possible attempt to keep your personal information safe, using the latest technology in Internet security. However, you should be aware that our site, as well as any other site, can potentially be a target for hackers. Please do not post any information on the site that is sensitive or that you would not want disclosed in case of an unlikely security breach. We use cookies to store information locally for advanced features of the site's operation, such as favorites, notes and alerts. Your personal details are not shared, rented or sold to any external companies or advertisers under any circumstances. Your credit card details and Paypal account details are only known to Paypal. You may review Paypal's agreements and policies for further information about how they safeguard your information. For security information, please visit: Paypal's security center .  For more information, please view Paypal's current <a href="#">privacy policy</a>.</p>
-                            </li>
-                            <li>
-                                <h3>Governing Law</h3>
-                                <p>These Terms shall be governed and construed in accordance with the laws of Ontario, Canada, without regard to its conflict of law provisions. Our failure to enforce any right or provision of these Terms will not be considered a waiver of those rights. If any provision of these Terms is held to be invalid or unenforceable by a court, the remaining provisions of these Terms will remain in effect. These Terms constitute the entire agreement between us regarding our Service, and supersede and replace any prior agreements we might have between us regarding the Service.</p>
-                            </li>
-                            <li>
-                                <h3>Contact Us</h3>
-                                <p>f you have any questions, concerns, or suggestions regarding this terms, please <a href="contact-us.html">contact us</a>.</p>
-                            </li>
-                        </ul>
+                            <ul class="list-unstyled termsList">
+                                <li>
+                                    <h3>Geltungsbereich</h3>
+                                    <p>Diese Datenschutzerklärung klärt Nutzer über die Art, den Umfang und Zwecke der Erhebung und Verwendung personenbezogener Daten durch den verantwortlichen Anbieter Dmitri Nuzbroh, Wienerstr. 24. 10999 Berlin, info@donatiq.com  (im folgenden “Anbieter”) auf dieser Website (im folgenden “Angebot” oder “Service”) auf.</p>
+                                    <p>Die rechtlichen Grundlagen des Datenschutzes finden sich im Bundesdatenschutzgesetz (BDSG) und dem Telemediengesetz (TMG).</p>
+                                </li>
+                                <li>
+                                    <h3>Zugriffsdaten/ Server-Logfiles</h3>
+                                    <p>Der Anbieter (beziehungsweise sein Webspace-Provider) erhebt Daten über jeden Zugriff auf das Angebot (so genannte Serverlogfiles). Zu den Zugriffsdaten gehören:</p>
+                                    <p>Name der abgerufenen Webseite, Datei, Datum und Uhrzeit des Abrufs, übertragene Datenmenge, Meldung über erfolgreichen Abruf, Browsertyp nebst Version, das Betriebssystem des Nutzers, Referrer URL (die zuvor besuchte Seite), IP-Adresse und der anfragende Provider., Standort (falls der Nutzer über die Systemfunktion des Gerätes explizit zugestimmt hat).
+Diese vorstehenden Zugriffsdaten sind ohne Personenbezug, d.h. bestimmten Personen nicht zuordenbar. Eine Zusammenführung dieser Daten mit anderen Datenquellen erfolgt nicht. </p>
+                                    <p>Der Anbieter verwendet die Protokolldaten nur für statistische Auswertungen zum Zweck des Betriebs Marktforschungs- und Marketingzwecken, der Sicherheit und der Optimierung des Angebotes. Der Anbieterbehält sich jedoch vor, die Protokolldaten nachträglich zu überprüfen, wenn aufgrund konkreter Anhaltspunkte der berechtigte Verdacht einer rechtswidrigen Nutzung besteht.</p>
+                                </li>
+                                <li>
+                                    <h3>Personenbezogene Daten</h3>
+                                    <p>Personenbezogene Daten werden nur erhoben und verarbeitet, wenn diese bei der Registrierung vom Nutzer freiwillig mitgeteilt werden. Personenbezogene Daten sind Informationen, aufgrund derer eine Person direkt oder indirekt identifiziert werden kann. Für die Registrierung ist die Angabe personenbezogener Daten erforderlich. Im Einzelnen umfasst dies folgende Daten:.</p>
+                                    <p>Vorname<br>
+                                        Nachname<br>
+                                        Anrede<br>
+                                        E-Mail-Adresse<br>
+                                        Gegebenenfalls weitere freiwillige Angaben, zum Beispiel:<br>
+                                        Straße und Hausnummer<br>
+                                        PLZ und Ort<br>
+                                        Telefonnummer<br>
+                                        Geburtsdatum<br>
+                                        Lieblings-Onlineshops<br>
+                                        Lieblingsvereine<br>
+                                        Diese Daten, mit Ausnahme von Vornamen des Nutzers, der standardmäßig auf Angebotsseiten neben einem selbst ausgesuchten Avatar dargestellt wird, werden von Anbieter streng vertraulich behandelt und Dritten nicht zugänglich gemacht. eingestellt hast) und mit Vornamen angezeigt.</p>
+                                </li>
+                                <li>
+                                    <h3>Nachverfolgung für die Zuteilung von Prämien</h3>
+                                    <p>Für die Ermittlung und Zuordnung von Cashback (bzw. Provisionen) an Nutzer ist es erforderlich, den Weg des Nutzers zum Partnerunternehmen zu verfolgen. Der Anbieter hat somit teilweise Kenntnis von der über das Angebot generierten Transaktionsvorgang bzw. Geschäfte des Nutzers. Der Anbieter kann diese Informationen für interne werbliche Zwecke nutzen, um beispielsweise ortsspezifische Produkt- bzw. Dienstleistungsangebote dem Nutzer im Rahmen des Angebots anzuzeigen. Diese Daten werden nur für interne Zwecke verwendet und werden nicht an Dritte weitergegeben.</p>
+                                </li>
+                                <li>
+                                    <h3>Verwendung von Cookies</h3>
+                                    <p>Cookies sind kleine Dateien, die es ermöglichen, auf dem Zugriffsgerät der Nutzer (PC, Smartphone o.ä.) spezifische, auf das Gerät bezogene Informationen zu speichern. Sie dienen zum einem der Benutzerfreundlichkeit von Webseiten und damit den Nutzern (z.B. Speicherung von Logindaten). Zum anderen dienen sie, um die statistische Daten der Webseitennutzung zu erfassen und sie zwecks Verbesserung des Angebotes analysieren zu können. Die Nutzer können auf den Einsatz der Cookies Einfluss nehmen. Die meisten Browser verfügen eine Option mit der das Speichern von Cookies eingeschränkt oder komplett verhindert wird. </p>
+                                    <p>Allerdings wird darauf hingewiesen, dass die Funktionalität des Angebots ohne Cookies eingeschränkt werden kann. Denn die Speicherung von Informationen in sog. Langzeit-Cookies dient dazu, um die durch den Einkauf über der Donatiq-Plattform generiertes Cashback den Nutzern und später dem Verein zuordnen zu können. </p>
+                                </li>
+                                <li>
+                                    <h3>Umgang mit personenbezogenen Daten und SSL-Verschlüsselung</h3>
+                                    <p>Personenbezogene Daten sind Informationen, mit deren Hilfe eine Person bestimmbar ist, also Angaben, die zurück zu einer Person verfolgt werden können. Dazu gehören der Name, die Emailadresse oder die Telefonnummer. Aber auch Daten über Vorlieben, Hobbies, Mitgliedschaften oder welche Webseiten von jemandem angesehen wurden zählen zu personenbezogenen Daten.</p>
+                                    <p>Personenbezogene Daten werden von dem Anbieter nur dann erhoben, genutzt und weiter gegeben, wenn dies gesetzlich erlaubt ist oder die Nutzer in die Datenerhebung einwilligen.</p>
+                                    <p>Diese Seite nutzt aus Gründen der Sicherheit und zum Schutz der Übertragung vertraulicher Inhalte, eine SSL-Verschlüsselung. Eine verschlüsselte Verbindung erkennen Sie daran, dass die Adresszeile des Browsers von "http://" auf "https://" wechselt und an dem Schloss-Symbol in Ihrer Browserzeile. Wenn die SSL Verschlüsselung aktiviert ist, können die Daten, die Sie an uns übermitteln, nicht von Dritten mitgelesen werden.</p>
+                                </li>
+                                <li>
+                                    <h3>Einsatz von Google Analytics</h3>
+                                    <p>Der Anbieter setzt Google Analytics, einen Webanalysedienst der Google Inc. („Google“) ein. Google verwendet Cookies. Die durch das Cookie erzeugten Informationen über die Benutzung des Onlineangebotes durch die Nutzer werden in der Regel an einen Server von Google in den USA übertragen und dort gespeichert.</p>
+                                    <p>Google benutzt diese Informationen im Auftrag des Anbieters, um die Nutzung des Onlineangebotes durch die Nutzer auszuwerten und gegenüber den Nutzern zu verbessern. Dabei können aus den verarbeiteten Daten pseudonyme Nutzungsprofile der Nutzer erstellt werden.</p>
+                                    <p>Die Nutzer können die Speicherung der Cookies durch eine entsprechende Einstellung ihrer Browser-Software verhindern; die Nutzer können darüber hinaus die Erfassung der durch das Cookie erzeugten und auf ihre Nutzung des Onlineangebotes bezogenen Daten an Google sowie die Verarbeitung dieser Daten durch Google verhindern, indem sie das unter dem folgenden Link verfügbare Browser-Plugin herunterladen und installieren: http://tools.google.com/dlpage/gaoptout?hl=de.</p>
+                                    <p>Weitere Informationen zur Datennutzung zu Werbezwecken durch Google, Einstellungs- und Widerspruchsmöglichkeiten erfahren Sie auf den Webseiten von Google: https://www.google.com/intl/de/policies/privacy/partners/ („Datennutzung durch Google bei Ihrer Nutzung von Websites oder Apps unserer Partner“), http://www.google.com/policies/technologies/ads („Datennutzung zu Werbezwecken“), http://www.google.de/settings/ads („Informationen verwalten, die Google verwendet, um Ihnen Werbung einzublenden“) und http://www.google.com/ads/preferences/ („Bestimmen Sie, welche Werbung Google Ihnen zeigt“).</p>
+                                </li>
+                                <li>
+                                    <h3>Newsletter</h3>
+                                    <p>Wenn Sie den Newsletter empfangen möchten, benötigen wir von Ihnen eine valide Email-Adresse sowie Informationen, die uns die Überprüfung gestatten, dass Sie der Inhaber der angegebenen Email-Adresse sind bzw. deren Inhaber mit dem Empfang des Newsletters einverstanden ist. Weitere Daten werden nicht erhoben. </p>
+                                    <p>Diese Daten werden nur für den Versand der Newsletter verwendet und werden nicht an Dritte weiter gegeben.</p>
+                                    <p>Mit der Anmeldung zum Newsletter speichern wir Ihre IP-Adresse und das Datum der Anmeldung. Diese Speicherung dient alleine dem Nachweis im Fall, dass ein Dritter eine Emailadresse missbraucht und sich ohne Wissen des Berechtigten für den Newsletterempfang anmeldet.</p>
+                                    <p>Ihre Einwilligung zur Speicherung der Daten, der Email-Adresse sowie deren Nutzung zum Versand des Newsletters können Sie jederzeit widerrufen. Der Widerruf kann über einen Link in den Newslettern selbst, in Ihrem Profilbereich oder per Mitteilung an die oben stehenden Kontaktmöglichkeiten erfolgen.</p>
+                                </li>
+                                <li>
+                                    <h3>Einbindung von Diensten und Inhalten Dritter</h3>
+                                    <p>Es kann vorkommen, dass innerhalb dieses Onlineangebotes Inhalte Dritter, wie zum Beispiel Videos von YouTube, Kartenmaterial von Google-Maps, RSS-Feeds oder Grafiken von anderen Webseiten eingebunden werden. Dies setzt immer voraus, dass die Anbieter dieser Inhalte (nachfolgend bezeichnet als "Dritt-Anbieter") die IP-Adresse der Nutzer wahr nehmen. Denn ohne die IP-Adresse, könnten sie die Inhalte nicht an den Browser des jeweiligen Nutzers senden. Die IP-Adresse ist damit für die Darstellung dieser Inhalte erforderlich. </p>
+                                </li>
+                                <li>
+                                    <h3>Kontaktaufnahme</h3>
+                                    <p>Bei der Kontaktaufnahme mit dem Anbieter (zum Beispiel per Kontaktformular oder E-Mail) werden die Angaben des Nutzers zwecks Bearbeitung der Anfrage sowie für den Fall, dass Anschlussfragen entstehen, gespeichert.</p>
+                                </li>
+                                <li>
+                                    <h3>Externe Links</h3>
+                                    <p>Der Anbieter hat keinen Einfluss auf die Inhalte externer Link, insbesondere Werbung von Dritten, die auf dem Angebot enthalten sind. Hierfür ist stets der jeweilige Anbieter oder Betreiber der Seite verantwortlich. Sollten rechtswidrige Seiten über die Hyperlinks abrufbar sein, bitten wir um Mitteilung an datenschutz@donatiq.com. </p>
+                                </li>
+                                <li>
+                                    <h3>Widerruf, Änderungen, Berichtigungen und Aktualisierungen</h3>
+                                    <p>Der Nutzer hat das Recht, auf Antrag unentgeltlich Auskunft zu erhalten über die personenbezogenen Daten, die über ihn gespeichert wurden. Zusätzlich hat der Nutzer das Recht auf Berichtigung unrichtiger Daten, Sperrung und Löschung seiner personenbezogenen Daten, soweit dem keine gesetzliche Aufbewahrungspflicht entgegensteht.</p>
+                                </li>
+                                <li>
+                                    <h3>Änderungen der Datenschutzbestimmungen</h3>
+                                    <p>Der Anbieter behält sich das Recht vor, diese Datenschutzbestimmungen jederzeit unter Beachtung der geltenden Datenschutzbestimmungen zu ändern. Über die Änderungen werden die registrierten Nutzer per E-Mail vor In-Kraft-Treten informiert.</p>
+                                </li>
+                                <li>
+                                    <h3>Datenschutzrechtliche Einwilligung</h3>
+                                    <p>Der Nutzer des Angebots willigt mit dieser Erklärung ausdrücklich ein, dass eine Erhebung, Verarbeitung und Nutzung der personenbezogenen Daten vom Anbieter für die oben ausgeführten Zwecke sowie für eigene Marketingzwecke im gesetzlich zulässigen Rahmen vorgenommen werden kann und diese Datenschutzbestimmungen zur Kenntnis genommen und akzeptiert worden sind. Die Einwilligung hierzu kann jederzeit schriftlich, auch per E-Mail an datenschutz@donatiq.com für die Zukunft widerrufen werden.</p>
+                                </li>
+                            </ul>
+
                     </div>
                 </div>
             </div>
