94477e8099957ec19181c5769dc6adcd81978c6c
Paul <yakushenkovpaul@gmail.com>
2018-02-16 16:10:11 +0300

    da

--- app/Services/UserService.php
+++ app/Services/UserService.php
@@ -477,8 +477,6 @@ class UserService
 
     public function referrals($program)
     {
-        if(!$program)   return false;
-
         $user = request()->user();
 
         return ReferralLink::getReferral($user, $program)->relationships()->paginate(env('PAGINATE', 25));
