1bc5b40c2b8c42e43cac01d48b8eaaacdd780524
dnuz <34270442+dnuz@users.noreply.github.com>
2018-02-19 18:40:06 +0100

    Update noclub.blade.php

--- resources/views/user/dashboard/noclub.blade.php
+++ resources/views/user/dashboard/noclub.blade.php
@@ -1,5 +1,5 @@
 <section class="clearfix bg-dark equalHeight dashboardSection">
     <div class="container">
-        <div class="well text-center">Um DonatiQ optimal zu nutzen, empfehlen wir Dir zuerst Deinen <a href="{!! url('/clubs') !!}">Lieblingsverein auszuwählen</a></div>
+        <div class="well text-center">Um Donatiq optimal nutzen zu können, empfehlen wir Dir zuerst Deinen <a href="{!! url('/clubs') !!}">Lieblingsverein</a> auszuwählen.</div>
     </div>
 </section>
